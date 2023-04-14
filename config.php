a0dcb25cc4d8b9f8da77acdff44ea8154e5c4776
Aahwani singh <ashwanisingh7930@gmail.com>
2023-04-14 13:49:01 +0530

    This is file of database connection to site

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ define("DB_SERVER", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
 define("DB_NAME", "registered");
+# Here you can use the Database name (registered) same as it is or you can change
 
 # Connection
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
