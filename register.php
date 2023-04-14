c02d7c0cc16a1b677ec432d82e1d9818a9611806
Aahwani singh <ashwanisingh7930@gmail.com>
2023-04-14 13:50:55 +0530

    This is Sign Up page File

--- register.php
+++ register.php
@@ -1,7 +1,6 @@
 <?php
 # Include connection
 require_once "./config.php";
-
 # Define variables and initialize with empty values
 $username_err = $email_err = $password_err = "";
 $username = $email = $password = "";
