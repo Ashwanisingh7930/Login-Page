b523ea7ee789cc5d7fc68e6075b8a3b587419765
Aahwani singh <ashwanisingh7930@gmail.com>
2023-04-14 13:50:10 +0530

    This is the index file

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
 # Initialize the session
 session_start();
-
 # If user is not logged in then redirect him to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== TRUE) {
   echo "<script>" . "window.location.href='./login.php';" . "</script>";
   exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
